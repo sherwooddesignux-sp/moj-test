a7aba681dd509e555b16a5b86e04cefcea91f4e1
sherwooddesignux-sp <sherwood.design.ux@gmail.com>
2026-03-22 20:07:09 +0000

    Play around with auth, case details page

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,19 @@
 // https://prototype-kit.service.gov.uk/docs/create-routes
 //
 
-const govukPrototypeKit = require('govuk-prototype-kit')
-const router = govukPrototypeKit.requests.setupRouter()
+const govukPrototypeKit = require("govuk-prototype-kit");
+const router = govukPrototypeKit.requests.setupRouter();
 
 // Add your routes here
+router.get("/case-details/:id", function (req, res) {
+  // 1. Get the ID from the URL (e.g., the '0' in /case-details/0)
+  const personId = req.params.id;
+
+  // 2. Fetch that specific person from your session data
+  const person = req.session.data["cases"][personId];
+
+  // 3. Send that person's data to the new page
+  res.render("case-details.html", {
+    person: person,
+  });
+});
