dac3fc88241847d71eea3bc6b3ac1347f78b9ad5
sherwooddesignux-sp <sherwood.design.ux@gmail.com>
2026-03-22 07:10:04 +0000

    updated data file, added table makeup

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,46 @@
 module.exports = {
-
-  // Insert values here
-
-}
+  cases: [
+    {
+      name: "Smith, Joe",
+      id: "A1234BC",
+      location: "Staffordshire",
+      status: "In progress",
+      tag: "blue",
+    },
+    {
+      name: "Jones, Sam",
+      id: "B5678DE",
+      location: "St. Helens",
+      status: "Pending",
+      tag: "yellow",
+    },
+    {
+      name: "Doe, John",
+      id: "C9012FG",
+      location: "Manchester",
+      status: "Completed",
+      tag: "green",
+    },
+    {
+      name: "Best, John",
+      id: "C0010FG",
+      location: "Swansea",
+      status: "Completed",
+      tag: "grey",
+    },
+    {
+      name: "Sherwood, Bobby",
+      id: "E1005FZ",
+      location: "Liverpool",
+      status: "Completed",
+      tag: "green",
+    },
+    {
+      name: "Smith, John",
+      id: "H100EFG",
+      location: "Birmingham",
+      status: "Urgent",
+      tag: "red",
+    },
+  ],
+};
